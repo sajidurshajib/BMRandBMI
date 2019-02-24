ef3479a9a0265d0ab7bc4bf8541ea70d4228fbde
sajidurshajib <sajidur.blog@gmail.com>
2019-02-24 19:03:46 +0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BMRandBMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
